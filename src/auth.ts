eb032820c92c355d24d2689f77e4cf6a70b6cd67
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2025-01-13 04:30:04 +0000

    chore: added amplify backend & adjusted AuthJS settings

--- src/auth.ts
+++ src/auth.ts
@@ -28,5 +28,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session
     }
   },
-  secret: process.env.AUTH_SECRET
+  secret: process.env.AUTH_SECRET,
+  trustHost: true
 });
